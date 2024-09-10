5443b2e892eb70ea35e99a1e795277d2d7b35308
fatihbaydar <fatihbaydar2004@gmail.com>
2024-09-10 07:36:37 +0300

    components created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import "bootstrap/dist/css/bootstrap.min.css";
 // index.html e de bootstrap in js teşekkürünü script olarak ekledik
-import Home from "./pages/Home";
 
 
 
@@ -9,7 +8,7 @@ import Home from "./pages/Home";
 function App() {
   return (
     <div >
-   <Home/>
+   App
     </div>
   );
 }
